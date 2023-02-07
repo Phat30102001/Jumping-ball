be67d9ff1d303a0cbfffdd7b4799da9de98d8848
Phat30102001 <lyminhphat30102001@gmail.com>
2023-02-07 20:23:38 +0700

    Second Commit

--- Assets/Script/PlatformSpawner.cs
+++ Assets/Script/PlatformSpawner.cs
@@ -7,7 +7,7 @@ public class PlatformSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test
     }
 
     // Update is called once per frame
